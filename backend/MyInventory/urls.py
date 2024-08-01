18c4ed5034e0d78157085f2b655353f73f2eff69
ajin-em <ajinem2@gmail.com>
2024-08-01 15:31:35 +0530

    urls

--- backend/MyInventory/urls.py
+++ backend/MyInventory/urls.py
@@ -17,13 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
 
 if settings.DEBUG:
