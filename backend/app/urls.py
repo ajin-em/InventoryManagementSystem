796dd951baff5526b24164f9902e130bace47270
ajin-em <ajinem2@gmail.com>
2024-08-01 13:59:09 +0530

    changes in routing at backend

--- backend/app/urls.py
+++ backend/app/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
-from .views import ProductView, ProductUpdateView
+from user.views import *
+from .views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('signup/', UserSignupView.as_view(), name='user-signup'),
+    path('signin/', UserSigninView.as_view(), name='user-signin'),
+    path('me/', UserDetailView.as_view(), name='user-detail'),
     path('products/', ProductView.as_view(), name='product'),
     path('products/<uuid:pk>/', ProductUpdateView.as_view(), name='product-detail'),
 ]
